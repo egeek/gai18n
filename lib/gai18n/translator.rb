6ae8337e611ea21abba17100b38d78832396d7d3
Stephen Houston <stephen.houston@teamfeepay.com>
2024-10-01 22:33:25 +0100

    Remove threading

--- lib/gai18n/translator.rb
+++ lib/gai18n/translator.rb
@@ -1,10 +1,14 @@
 module GAI18n
   class Translator
     def translate
-      threads = Locale.target_locale_files.map do |target_locale_file|
-        Thread.new { target_locale_file.translate }
+      # threads = Locale.target_locale_files.map do |target_locale_file|
+      #   Thread.new { target_locale_file.translate }
+      # end
+      # threads.each(&:join)
+      Locale.target_locale_files.map do |target_locale_file|
+        puts "Translating #{target_locale_file}..."
+        target_locale_file.translate
       end
-      threads.each(&:join)
     end
   end
 end
